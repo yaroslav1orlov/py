0f2af189d510e6bbc5ce28ee1e14357913608378
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-07-02 17:03:08 -0600

    Add demonstration of basic data types in Python

---
+++ Programs/Program_3.py
@@ -0,0 +1,11 @@
+bool_data = True
+int_data = 42
+float_data = 3.14
+complex_data = 2 + 3j
+str_data = "Hello, World!"
+
+print(f"Boolean data: {bool_data},    type: {type(bool_data)}")
+print(f"Integer data: {int_data},     type: {type(int_data)}")
+print(f"Float data:   {float_data},   type: {type(float_data)}")
+print(f"Complex data: {complex_data}, type: {type(complex_data)}")
+print(f"String data: '{str_data}',    type: {type(str_data)}")

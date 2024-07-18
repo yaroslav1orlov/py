bd1573a4fae50e468960e29dd72eae792885db9a
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-07-18 14:01:31 -0600

    made 2 comits

---
+++ Programs/Program_4.py
@@ -0,0 +1,9 @@
+def say_hello(): 
+    yourName = input("Enter you name: ")
+    print("Hello " + yourName + "!")
+    howAreYou = input("How are you? ")
+    print("I'm too thanks")
+    print("Have a nice day!")
+    input("")
+    
+say_hello()
---
+++ Python Basics/9Functions.py
@@ -0,0 +1,54 @@
+def say_hello():
+    print("Hello")
+
+def say_hello():
+    print("Hello")
+ 
+ 
+print("Bye")
+
+def say_hello():
+    print("Hello")
+ 
+ 
+say_hello()
+say_hello()
+say_hello()
+
+def say_hello(): print("Hello")
+ 
+say_hello()
+
+def say_hello():
+    print("Hello")
+ 
+ 
+def say_goodbye():
+    print("Good Bye")
+ 
+ 
+say_hello()
+say_goodbye()
+
+def print_messages():
+    def say_hello(): print("Hello")
+
+    def say_goodbye(): print("Good Bye")
+
+    say_hello()
+
+    say_goodbye()
+ 
+print_messages()
+
+def main():
+    say_hello()
+    say_goodbye()
+ 
+def say_hello():
+    print("Hello")
+ 
+def say_goodbye():
+    print("Good Bye")
+ 
+main()

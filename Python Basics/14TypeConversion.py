d37411f6b30e6d6ee4291d4a473725ea4da87fe3
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-07-19 11:55:21 -0600

    Added Type Conversion

---
+++ Python Basics/14TypeConversion.py
@@ -0,0 +1,47 @@
+a = 2
+b = 2.5
+c = a + b
+print(c)
+
+a = "2"
+b = 3
+c = a + b
+
+a = "2"
+b = 3
+c = int(a) + b
+print(c)
+
+a = int(15)
+b = int(3.7)
+c = int("4")
+e = int(False)
+f = int(True)
+
+b = int("a1c")
+c = int("4.7")
+
+a = "2.7"
+b = 3
+c = float(a) + b
+print(c)
+
+a = float(15)
+b = float(3.7)
+c = float("4.7")
+d = float("5")
+e = float(False)
+f = float(True)
+d = float("abc")
+a = str(False)
+b = str(True)
+c = str(5)
+d = str(5.7)
+
+age = 22
+message = "Age: " + age
+print(message)
+
+age = 22
+message = "Age: " + str(age)
+print(message)

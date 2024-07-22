4e17e448b3520d9a455d74f01c6b9fb252d9b053
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-07-22 16:44:27 -0600

    Added Program

---
+++ Programs/Program_5.py
@@ -0,0 +1,11 @@
+numb_1 = input("First number: ")
+numb_2 = input("Second number: ")
+
+number_1 = int(numb_1)
+number_2 = int(numb_2)
+
+print(f"{number_1} + {number_2} = {number_1 + number_2}")
+print(f"{number_1} - {number_2} = {number_1 - number_2}")
+print(f"{number_1} * {number_2} = {number_1 * number_2}")
+print(f"{number_1} / {number_2} = {number_1 / number_2}")
+print(f"{number_1} % {number_2} = {number_1 % number_2}")

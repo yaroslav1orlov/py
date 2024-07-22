49492d2bf9f15016bc15d4d5a260b50bcf3cf00a
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-07-22 16:59:27 -0600

    Added program

---
+++ Programs/Program_6.py
@@ -0,0 +1,31 @@
+def sum():
+    numb_1 = input("Sum|Enter first number: ")
+    numb_2 = input("Sum|Enter second number: ")
+
+    print(numb_1 + numb_2)
+def subtraction():
+    numb_11 = input("Subtraction|Enter first number: ")
+    numb_12 = input("Subtraction|Enter second number: ")
+
+    print(numb_11 + numb_12)
+def division():
+    numb_21 = input("Division|Enter first number: ")
+    numb_22 = input("Division|Enter second number: ")
+
+    print(numb_21 / numb_22)
+def multiplication():
+    numb_31 = input("Multiplication|Enter first number: ")
+    numb_32 = input("Multiplication|Enter second number: ")
+
+    print(numb_31 * numb_32)
+def remainderOfDivision():
+    numb_41 = input("Remainder of division|Enter first number: ")
+    numb_42 = input("Remainder of division|Enter second number: ")
+
+    print(numb_41 % numb_42)
+
+sum()
+subtraction()
+division()
+multiplication()
+remainderOfDivision()

e23811c7670bd32cc286d0f982b3815fd1ece4f4
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-08-07 12:54:13 -0600

    Added HW2

--- PythonMetanit/2.2_Variables_and_data_types/HW_2/10.py
+++ PythonMetanit/2.2_Variables_and_data_types/HW_2/10.py
@@ -1,3 +1,22 @@
-
-
 # Реализуйте простой счетчик с возможностью увеличения и сброса значения, используя цикл while и ввод пользователя
+
+counter = 0
+
+while True:
+    print(f"Current counter value: {counter}")
+    print("Enter '+', to increase the value by 1.")
+    print("Enter '0', to reset the value.")
+    print("Enter 'q', to quit.")
+    
+    user_input = input("Your choice: ")
+
+    if user_input == 'p':
+        counter += 1
+    elif user_input == 'r':
+        counter = 0
+    elif user_input == 'q':
+        break
+    else:
+        print("Invalid input, try again.")
+
+print("Program terminated.")
--- PythonMetanit/2.2_Variables_and_data_types/HW_2/9.py
+++ PythonMetanit/2.2_Variables_and_data_types/HW_2/9.py
@@ -1,3 +1,12 @@
-
-
 # Создайте программу, которая находит наименьший общий делитель двух чисел, используя цикл while
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+a = num1
+b = num2
+
+while b != 0:
+    a, b = b, a % b
+
+print("Least common divisor: " + a)

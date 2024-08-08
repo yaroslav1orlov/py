a37cc222ce64ceaf536748879730cc09a16b603b
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-08-08 13:27:47 -0600

    Added HW2

--- PythonMetanit/2.2_Variables_and_data_types/HW_2/7.py
+++ PythonMetanit/2.2_Variables_and_data_types/HW_2/7.py
@@ -2,13 +2,16 @@
 # - от 1 до 10. Используйте цикл while
 
 import random
-
 randomNumber = random.randint(1, 10)
 number = 0
+attempts = 3
 
 while number != randomNumber:
-    userInput = input("Guess the number from 1 to 10: ")
+    userInput = input(f"Guess the number from 1 to 10 (Attempts left: {attempts}): ")
     number = int(userInput)
-    print("Opss, try again ):")
-
-print("Congratulations you guessed the number (:")
+    attempts -= 1 # attempts = attempts - 1
+    print("Oops, try again ):" * (number != randomNumber) + "Congratulations, you guessed the number (:" * (number == randomNumber)) # == Mean True, False
+    if attempts == 0:
+        break
+    
+print("Sorry, you've used all your attempts. The number was", randomNumber * (number != randomNumber))

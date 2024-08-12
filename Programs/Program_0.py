1be6c5086f9f074bf24824da1a591b7ba8476117
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2024-08-12 13:07:11 -0600

    fix bug

--- Programs/Program_0.py
+++ Programs/Program_0.py
@@ -1,7 +1,13 @@
-SpellProg = "Hello"
+SpellProg = " Hello "
 
-print(SpellProg[0])
 print(SpellProg[1])
 print(SpellProg[2])
 print(SpellProg[3])
+print(SpellProg[4])
+print(SpellProg[5])
+
+print(SpellProg[0])
+
+print(SpellProg[4])
+print(SpellProg[5])
 print(SpellProg[4])

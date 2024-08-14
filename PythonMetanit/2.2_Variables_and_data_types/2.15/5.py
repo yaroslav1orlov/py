71d79f7915bfd841eec1d355e308f5e0fb29d139
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-08-14 11:31:20 -0600

    Done file 5

---
+++ PythonMetanit/2.2_Variables_and_data_types/2.15/5.py
@@ -0,0 +1,8 @@
+# 5. Напишите цикл for, который выводит таблицу умножения на 5.
+
+for i in range(2, 10):
+    print("")
+    print("   ", i)
+    print("")
+    for x in range(2, 10):
+        print(f"{i} * {x} =", i * x)    

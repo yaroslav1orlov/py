b8da602aa3f86321a896fc395770cdb69d7f86ff
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2024-08-16 00:26:57 -0600

    Done 8 | 12:26

--- PythonMetanit/2.15_FunctionParameters/8.py
+++ PythonMetanit/2.15_FunctionParameters/8.py
@@ -1,9 +1,13 @@
 # 8. Напишите программу, которая проверяет, содержит ли строка гласные буквы.
 
-def isVowels(str):
-    result = True
-    return result
-print
-str = "qwertyuioplkjhgfdsazxcvbnm"
+def isVowels(str1):
+    if any(char in "aeiouAEIOU" for char in str1): 
+        print("In string are some vowels letters")
+        return True 
+    print("In string aren't any vowels letters")  
+    return False
 
-result = isVowels(str)
+
+str1 = "YEROX_RUST"
+
+result = isVowels(str1) 

f67a0a40b5171665d2433bce52dc0bad51ab8520
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-08-19 15:37:57 -0600

    commit

--- PythonMetanit/2.2_VariablesAndDataTypes/HW/6 copy.py
+++ PythonMetanit/2.2_VariablesAndDataTypes/HW/6 copy.py
@@ -1,11 +1,10 @@
 # Объявите переменную text и присвойте ей строку "Python". Выведите первый символ строки
 
-
 text = "Python"
 txt = input("Enter symbol: ")
 
 str = ""
 for i in text:
-    str += i
+    str = str + i
 
 print(str)

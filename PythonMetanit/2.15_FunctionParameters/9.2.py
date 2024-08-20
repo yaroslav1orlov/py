da24d41be2bf988bfcf198e6e353c87a82a5883a
Yaroslav Orlov <yaroslav1orlov@hotmail.com>
2024-08-20 14:35:29 -0600

    comit

--- PythonMetanit/2.15_FunctionParameters/9.2.py
+++ PythonMetanit/2.15_FunctionParameters/9.2.py
@@ -1,3 +1,10 @@
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-print(num)
+c = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ 
+for c in c:
+    print(c)
+    num1 = c / 2
+    num2 = int(num1)
+    if num1 == num2:
+        print("чесло чётное")
+    else:
+        print("число не чётное")
